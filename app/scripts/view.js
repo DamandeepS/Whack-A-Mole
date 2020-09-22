21edfbbd9d99661b86a3a20fa9df2248e6d94f3d
Damandeep Singh <mail@damandeepsingh.com>
2020-09-23 02:10:37 +0530

    syntax :p

--- app/scripts/view.js
+++ app/scripts/view.js
@@ -1,5 +1,5 @@
 
-  (() => {
+(() => {
     const newGame = new WhackAMole();
 
     const gameContainerEl = document.querySelector('.game-c');
@@ -9,8 +9,8 @@
     const playGroundEl = gameContainerEl.querySelector('.game-js__playground');
     const restartButtonEl = gameContainerEl.querySelector('.game-js__restart-control');
     const pauseButtonEl = gameContainerEl.querySelector('.game-js__pause-control');
-    const startOverlayEl = gameContainerEl.querySelector('.game-c__start-overlay')
-    const startOverlayHighScoreEl = startOverlayEl.querySelector('.game-js__high-score')
+    const startOverlayEl = gameContainerEl.querySelector('.game-c__start-overlay');
+    const startOverlayHighScoreEl = startOverlayEl.querySelector('.game-js__high-score');
     const startControlButtonEls = startOverlayEl.querySelectorAll('.game-js__start-controls');
     const endScreenEl = gameContainerEl.querySelector('.game-js__end-screen');
     const endScreenScoreEl = endScreenEl.querySelector('.game-js__current-score');
@@ -19,17 +19,17 @@
 
 
     const _orientationChanged = () => {
-      gameContainerEl.classList.remove('game-c--landscape')
-      if (Math.abs(window.orientation) == 90) {
-        gameContainerEl.classList.add('game-c--landscape')
-      }
-    }
+        gameContainerEl.classList.remove('game-c--landscape');
+        if (Math.abs(window.orientation) == 90) {
+            gameContainerEl.classList.add('game-c--landscape');
+        }
+    };
 
     const restartGame = () => {
-      newGame.resetGame();
-      endScreenEl.classList.remove('game-c__end-screen--active');
-      startOverlayEl.classList.add('game-c__start-overlay--active');
-    }
+        newGame.resetGame();
+        endScreenEl.classList.remove('game-c__end-screen--active');
+        startOverlayEl.classList.add('game-c__start-overlay--active');
+    };
 
 
     highScoreEl.textContent = newGame.highScore;
@@ -37,16 +37,15 @@
 
     // Events
     Array.prototype.forEach.call(startControlButtonEls, button => {
-      button.addEventListener('click', () => {
-        const difficulty = button.dataset['difficulty'] || 'easy';
-        newGame.handleStartControl(difficulty);
-      })
+        button.addEventListener('click', () => {
+            const difficulty = button.dataset['difficulty'] || 'easy';
+            newGame.handleStartControl(difficulty);
+        });
     });
 
 
     window.addEventListener('orientationchange', _orientationChanged);
     _orientationChanged();
-
 
 
     pauseButtonEl.addEventListener('click', newGame.togglePause);
@@ -56,61 +55,61 @@
     endScreenReplayControlEl.addEventListener('click', restartGame);
 
     document.addEventListener('game-update-telemetry', () => {
-      timerEl.textContent = newGame.totalTime;
-      currentScoreEl.textContent = newGame.score;
+        timerEl.textContent = newGame.totalTime;
+        currentScoreEl.textContent = newGame.score;
     });
 
     document.addEventListener('game-ended', () => {
-      endScreenEl.classList.add('game-c__end-screen--active');
+        endScreenEl.classList.add('game-c__end-screen--active');
 
-      endScreenScoreEl.innerText = newGame.score;
-      highScoreEl.textContent = newGame.highScore;
-      currentScoreEl.textContent = newGame.score;
-      endScreenHighScoreEl.textContent = newGame.highScore;
-      
+        endScreenScoreEl.innerText = newGame.score;
+        highScoreEl.textContent = newGame.highScore;
+        currentScoreEl.textContent = newGame.score;
+        endScreenHighScoreEl.textContent = newGame.highScore;
 
-      newGame.resetGame();
 
-      setTimeout(() => {
-        if (window.confirm('Game Over!!!')) {
-          endScreenEl.classList.remove('game-c__end-screen--active');
-          startOverlayEl.classList.add('game-c__start-overlay--active');
-        }
-      }, 0);
+        newGame.resetGame();
+
+        setTimeout(() => {
+            if (window.confirm('Game Over!!!')) {
+                endScreenEl.classList.remove('game-c__end-screen--active');
+                startOverlayEl.classList.add('game-c__start-overlay--active');
+            }
+        }, 0);
     });
 
     document.addEventListener('game-reset', () => {
-      currentScoreEl.textContent = 0;
-      timerEl.textContent = newGame.totalTime;
-      pauseButtonEl.textContent = 'Pause';
-      while(playGroundEl.lastChild) {
-        playGroundEl.removeChild(playGroundEl.firstChild);
-      }
-      gameContainerEl.classList.remove('game-c--paused');
+        currentScoreEl.textContent = 0;
+        timerEl.textContent = newGame.totalTime;
+        pauseButtonEl.textContent = 'Pause';
+        while (playGroundEl.lastChild) {
+            playGroundEl.removeChild(playGroundEl.firstChild);
+        }
+        gameContainerEl.classList.remove('game-c--paused');
     });
 
     document.addEventListener('tiles-prepared', e => {
-      const size = e.detail.size;
-      startOverlayEl.classList.remove('game-c__start-overlay--active');
-      const fragement = document.createDocumentFragment();
-      newGame.tiles.forEach(tile => fragement.appendChild(tile));
+        const size = e.detail.size;
+        startOverlayEl.classList.remove('game-c__start-overlay--active');
+        const fragement = document.createDocumentFragment();
+        newGame.tiles.forEach(tile => fragement.appendChild(tile));
 
-      playGroundEl.classList.remove('game-c__playground-3');
-      playGroundEl.classList.remove('game-c__playground-4');
-      playGroundEl.classList.remove('game-c__playground-6');
-      playGroundEl.classList.add(`game-c__playground-${size}`);
+        playGroundEl.classList.remove('game-c__playground-3');
+        playGroundEl.classList.remove('game-c__playground-4');
+        playGroundEl.classList.remove('game-c__playground-6');
+        playGroundEl.classList.add(`game-c__playground-${size}`);
 
-      playGroundEl.appendChild(fragement);
+        playGroundEl.appendChild(fragement);
     });
 
     document.addEventListener('game-paused', () => {
-      if (newGame.gamePaused) {
-        pauseButtonEl.textContent = 'Resume';
-        gameContainerEl.classList.add('game-c--paused');
-      } else {
-        pauseButtonEl.textContent = 'Pause';
-        gameContainerEl.classList.remove('game-c--paused');
-      }
-    })
-    
-  })();
+        if (newGame.gamePaused) {
+            pauseButtonEl.textContent = 'Resume';
+            gameContainerEl.classList.add('game-c--paused');
+        } else {
+            pauseButtonEl.textContent = 'Pause';
+            gameContainerEl.classList.remove('game-c--paused');
+        }
+    });
+
+})();
--- app/scripts/whackAMole.js
+++ app/scripts/whackAMole.js
@@ -9,7 +9,7 @@ class WhackAMole {
             const selectedTile = e.detail;
             if (this.selectionEnabled && this.randomTile === selectedTile) {
                 this.score++;
-            } else if (this.selectionEnabled){
+            } else if (this.selectionEnabled) {
                 this.score--;
             }
 
@@ -37,7 +37,7 @@ class WhackAMole {
                 this.gameEnded();
             }
         }, 1000);
-    }
+    };
 
     togglePause = () => {
         this.gamePaused = !this.gamePaused;
@@ -48,11 +48,11 @@ class WhackAMole {
         } else {
             this.selectionEnabled = this.selectionPrevState;
         }
-    }
+    };
 
     _initGrid = (difficulty) => {
         this.resetGame();
-        let gridSize = 3;
+        let gridSize;
         switch (difficulty) {
             case 'easy':
             default:
@@ -66,7 +66,7 @@ class WhackAMole {
                 break;
         }
         this._drawGrid(gridSize);
-    }
+    };
 
     _randomizeHighlightedTile = () => {
         const randomIndex = Math.max(0, Math.min(this.tiles.length - 1, Math.round(Math.random() * this.tiles.length - 1)));
@@ -78,11 +78,11 @@ class WhackAMole {
                 setTimeout(() => tile.classList.add('game-c__game-tile--highlighted'), 50);
             }
         });
-    }
+    };
 
 
     _drawGrid = size => {
-        const totalTiles = size*size;
+        const totalTiles = size * size;
         while (this.tiles.length < totalTiles) {
             const tile = document.createElement('div');
             tile.classList.add('game-c__game-tile');
@@ -90,38 +90,38 @@ class WhackAMole {
                 e.preventDefault();
                 e.stopPropagation();
                 document.dispatchEvent(new CustomEvent('game__tile-clicked', {
-                    detail: tile
+                    detail: tile,
                 }));
             });
 
             tile.addEventListener('keydown', e => {
                 if (e.code == 'Enter' || e.code == 'Space') {
                     document.dispatchEvent(new CustomEvent('game__tile-clicked', {
-                        detail: tile
+                        detail: tile,
                     }));
                 }
-            })
+            });
             tile.setAttribute('role', 'button');
             tile.setAttribute('tabindex', '0');
             this.tiles.push(tile);
-        };
+        }
 
         document.dispatchEvent(new CustomEvent('tiles-prepared', {
             detail: {
-                size
+                size,
             }
         }));
-    } 
+    };
 
     gameEnded = () => {
         if (this.score > this.highScore) {
-            this.highScore = this.score
-            window.localStorage.setItem('game-highscore', `${this.score}`)
+            this.highScore = this.score;
+            window.localStorage.setItem('game-highscore', `${this.score}`);
         }
 
         document.dispatchEvent(new CustomEvent('game-ended'));
 
-    }
+    };
 
     resetGame = () => {
         this.score = 0;
@@ -133,6 +133,6 @@ class WhackAMole {
         window.clearInterval(this.interval);
         this.selectionEnabled = false;
         document.dispatchEvent(new CustomEvent('game-reset'));
-    }
+    };
 
 }
